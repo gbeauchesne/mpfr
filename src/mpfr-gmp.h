gbeauchesne/mpfr

commit fb161e837db685f84a563e0024f832a759500a6a

Check that the numberof() macro is used on an array.   * src/mpfr-gmp.h: define numberof_const() macro, equivalent to the     old numberof() macro: no check, but constant expression; improved     the numberof() macro to check that it is used on an array (since     an array can silently be converted to a pointer in C). Note that     potential failures are currently reported at run time.   * src/mulders.c: replaced numberof by numberof_const in #define's     (also removed a useless cast at the same time).
git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfr/trunk@10964 280ebfd0-de03-0410-8827-d642c229c3f4

src/mpfr-gmp.h
@@ -246,11 +246,34 @@ __MPFR_DECLSPEC extern const struct bases mpfr_bases[257];
 #undef ABS
 #undef MIN
 #undef MAX
-#undef numberof
 #define ABS(x) ((x) >= 0 ? (x) : -(x))
 #define MIN(l,o) ((l) < (o) ? (l) : (o))
 #define MAX(h,i) ((h) > (i) ? (h) : (i))
-#define numberof(x)  (sizeof (x) / sizeof ((x)[0]))
+
+/* Size of an array, as a constant expression. */
+#define numberof_const(x)  (sizeof (x) / sizeof ((x)[0]))
+
+/* Size of an array, safe version but not a constant expression:
+   Since an array can silently be converted to a pointer, we check
+   that this macro is applied on an array, not a pointer. */
+#undef numberof
+#if 0
+/* The following should work with GCC as documented in its manual,
+   but fails: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=38377#c10
+   Thus disabled for now. */
+# define numberof(x)                                                    \
+  ( __extension__ ({                                                    \
+      int is_array = (void *) &(x) == (void *) &(x)[0];                 \
+      MPFR_STAT_STATIC_ASSERT (__builtin_constant_p (is_array) ?        \
+                               is_array : 1);                           \
+      MPFR_ASSERTN (is_array);                                          \
+      numberof_const (x);                                               \
+    }) )
+#else
+# define numberof(x)                                    \
+  (MPFR_ASSERTN ((void *) &(x) == (void *) &(x)[0]),    \
+   numberof_const (x))
+#endif
 
 /* Allocate func are defined in gmp-impl.h.
    Warning: the code below fetches the GMP memory allocation functions the first

src/mulders.c
@@ -41,7 +41,7 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 static short mulhigh_ktab[MPFR_MULHIGH_TAB_SIZE];
 #else
 static short mulhigh_ktab[] = {MPFR_MULHIGH_TAB};
-#define MPFR_MULHIGH_TAB_SIZE ((mp_size_t) numberof (mulhigh_ktab))
+#define MPFR_MULHIGH_TAB_SIZE (numberof_const (mulhigh_ktab))
 #endif
 
 /* Put in  rp[n..2n-1] an approximation of the n high limbs
@@ -151,7 +151,7 @@ mpfr_mullow_n (mpfr_limb_ptr rp, mpfr_limb_srcptr np, mpfr_limb_srcptr mp,
 static short sqrhigh_ktab[MPFR_SQRHIGH_TAB_SIZE];
 #else
 static short sqrhigh_ktab[] = {MPFR_SQRHIGH_TAB};
-#define MPFR_SQRHIGH_TAB_SIZE (numberof (sqrhigh_ktab))
+#define MPFR_SQRHIGH_TAB_SIZE (numberof_const (sqrhigh_ktab))
 #endif
 
 /* Put in  rp[n..2n-1] an approximation of the n high limbs
@@ -192,7 +192,7 @@ mpfr_sqrhigh_n (mpfr_limb_ptr rp, mpfr_limb_srcptr np, mp_size_t n)
 static short divhigh_ktab[MPFR_DIVHIGH_TAB_SIZE];
 #else
 static short divhigh_ktab[] = {MPFR_DIVHIGH_TAB};
-#define MPFR_DIVHIGH_TAB_SIZE (numberof (divhigh_ktab))
+#define MPFR_DIVHIGH_TAB_SIZE (numberof_const (divhigh_ktab))
 #endif
 
 #if !(defined(WANT_GMP_INTERNALS) && defined(HAVE___GMPN_SBPI1_DIVAPPR_Q))

commit 12cfe8ff33eba2b1caec61efb489e02eeb3adc3b

[src] Fixed bug introduced in r10964 with --with-gmp-build: move the numberof_const() definition from mpfr-gmp.h to mpfr-impl.h.
git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfr/trunk@11002 280ebfd0-de03-0410-8827-d642c229c3f4

src/mpfr-gmp.h
@@ -250,9 +250,6 @@ __MPFR_DECLSPEC extern const struct bases mpfr_bases[257];
 #define MIN(l,o) ((l) < (o) ? (l) : (o))
 #define MAX(h,i) ((h) > (i) ? (h) : (i))
 
-/* Size of an array, as a constant expression. */
-#define numberof_const(x)  (sizeof (x) / sizeof ((x)[0]))
-
 /* Size of an array, safe version but not a constant expression:
    Since an array can silently be converted to a pointer, we check
    that this macro is applied on an array, not a pointer. */

src/mpfr-impl.h
@@ -1439,6 +1439,9 @@ do {                                                                  \
       MPFR_MANT(y) = MPFR_MANT(x);              \
     } while (0)
 
+/* Size of an array, as a constant expression. */
+#define numberof_const(x)  (sizeof (x) / sizeof ((x)[0]))
+
 
 /******************************************************
  ************  Save exponent/flags macros  ************